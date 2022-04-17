ef22b0230aa4b48ad113c28bae1b9f4457469092
FayZerDv <40219975+wfayZerDev@users.noreply.github.com>
2022-04-17 21:36:10 +0200

    Update Banner.js

--- aouv/src/components/Banner.js
+++ aouv/src/components/Banner.js
@@ -5,9 +5,6 @@ function Banner() {
     const title = 'Aouv.fr'
     return (
         <div className='lmj-banner'>
-            <a href=''>Lien 1</a>
-            <a href=''>Lien 2</a>
-            <a href=''>Lien 3</a>
             <img src={logo} alt='Aouv.fr' className='lmj-logo' />
             <h1 className='lmj-title'>{title}</h1>
         </div>
